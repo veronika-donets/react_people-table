3819b88a5b4d08c3e17c5865dc43b1a3a15f16e0
Veronika D <donets.veronika@yahoo.com>
2019-11-11 21:09:13 +0200

    Updates

---
+++ precache-manifest.9806fd443238bc2e5f344bbcceb89259.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc6ca3028e57f40486dd56e78a11bcc",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "9d5c46dbedd4239fd22c",
+    "url": "/react_people-table/static/css/main.53a01f42.chunk.css"
+  },
+  {
+    "revision": "023c3e72810661bc96b8",
+    "url": "/react_people-table/static/js/2.84d2f825.chunk.js"
+  },
+  {
+    "revision": "9d5c46dbedd4239fd22c",
+    "url": "/react_people-table/static/js/main.586813e9.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
--- src/components/Api.js
+++
@@ -1,6 +0,0 @@
-const loadedPeople = () => (
-  fetch('https://mate-academy.github.io/react_people-table/api/people.json')
-    .then(response => response.json())
-);
-
-export default loadedPeople;
